08146e9dcf4d6d3d66328fd671bb52c225448f62
DieR-G <00043920@uca.edu.sv>
2021-03-11 15:23:09 -0600

    subtract function added

--- Program.cs
+++ Program.cs
@@ -14,12 +14,12 @@ namespace intro_a_github
             Console.WriteLine("Enter a value for b: ");
             b = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)} ");
+            Console.WriteLine($"Subtracting {a} + {b} equals {SubNumbers(a, b)} ");
         }
 
-        public static int AddNumbers(int a, int b)
+        public static int SubNumbers(int a, int b)
         {
-            return a + b;
+            return a - b;
         }
     }
 }
